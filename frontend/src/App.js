8985e05e4f22c0fd42bb2e0005c09b2691c193f1
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-10 20:48:45 +0545

    simple dapp to read and update greeter function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,18 +13,49 @@ function App() {
   useEffect(() => {
 
     const loadProvider = async () => {
-      let contractAddress = "";
-      const url = "http://127.0.0.1:7545";
-    }
-  })
+      let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const url = "http://localhost:8545";
+      const provider = new ethers.providers.JsonRpcProvider(url);
 
+      const contract = new ethers.Contract(
+        contractAddress,
+        Greeter.abi,
+        provider
+      );
+      setContract(contract);
+      setProvider(provider);
+      // console.log(contract)
+    };
+    loadProvider();
+  }, [])
+
+  useEffect(() => {
+    const getGreeting = async () => {
+      // console.log(`dgdssdag`)
+      const greeting = await contract.greet();
+      // console.log(greeting);
+      doGreeting(greeting);
+    }
+    contract && getGreeting();
+  }, [contract])
+
+  const changeGreeting = async () => {
+    const value = document.getElementById('value').value;
+    const signer = provider.getSigner();
+    const contractWithSigner = contract.connect(signer);
+    const transaction = await contractWithSigner.setGreeting(value);
+    await transaction.wait();
+    const greeting = await contract.greet();
+    doGreeting(greeting);
+
+  }
   return (
     <div className="center">
-      dsdgsg
+      <h3>{greeting}</h3>
+      <input type="text" className="input" id='value'></input>
+      <button className="button" onClick={changeGreeting}>Change</button>
     </div>
   );
 }
 
 export default App;
-
-
